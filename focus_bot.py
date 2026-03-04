d16502085eda268cbd46952b99bbaea2276a0ecd
indecision44 <indecision4@mail.ru>
2026-03-04 12:08:30 +0300

    добавление тестовых команд

--- focus_bot.py
+++ focus_bot.py
@@ -1,8 +1,44 @@
-from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
 import random
 import datetime
 
 TOKEN = '8549484882:AAFr6klJedHwGZJnCeE60fsaccI8cNS7CLQ'
+
+######################
+#ТЕСТОВЫЕ КОМАНДЫ OPEN
+######################
+async def test_join(update, context):
+    """Имитирует вход нового участника"""
+    test_name = "ТестовыйНовичок"
+    
+    await update.message.reply_text("🧪 **ТЕСТ ВХОДА**\nИмитирую появление нового участника...")
+    
+    greeting = random.choice(GREETINGS).format(name=test_name)
+    await update.message.reply_text(greeting)
+    await update.message.reply_text(MANDATORY_GREETING)
+    
+    await update.message.reply_text(
+        f"👋 {test_name}, что тебя интересует?",
+        reply_markup=get_welcome_keyboard()
+    )
+    
+    time_now = datetime.datetime.now().strftime("%H:%M:%S")
+    print(f"🧪 [{time_now}] Тест входа от {update.effective_user.first_name}")
+
+async def test_leave(update, context):
+    """Имитирует выход участника"""
+    test_name = "ТестовыйНовичок"
+    
+    await update.message.reply_text("🧪 **ТЕСТ ВЫХОДА**\nИмитирую уход участника...")
+    
+    farewell = random.choice(FAREWELLS).format(name=test_name)
+    await update.message.reply_text(farewell)
+    
+    time_now = datetime.datetime.now().strftime("%H:%M:%S")
+    print(f"🧪 [{time_now}] Тест выхода от {update.effective_user.first_name}")
+######################
+#ТЕСТОВЫЕ КОМАНДЫ CLOSE
+######################
 
 # Приветствия для спортзала
 GREETINGS = [
@@ -59,4 +95,5 @@ if __name__ == '__main__':
     
     app = Application.builder().token(TOKEN).build()
     app.add_handler(MessageHandler(filters.StatusUpdate.ALL, track_gym_members))
+    app.add_handler(CommandHandler("test1", test_join))  # имитация входа
     app.run_polling()
